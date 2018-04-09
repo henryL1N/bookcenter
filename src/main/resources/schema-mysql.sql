52cd2e2494a9743024d49aa58ee2ea8e66be98be
Henry Lin <badcop@163.com>
2018-04-09 09:41:29 +0800

    up

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -1,40 +1,38 @@
-
-
-DROP TABLE if exists `employee` CASCADE;
-DROP TABLE if exists `department` CASCADE;
-DROP TABLE if exists `book` CASCADE;
-DROP TABLE if exists `publisher` CASCADE;
-DROP TABLE if exists `bookcenter` CASCADE;
-DROP TABLE if exists `warehouse` CASCADE;
-DROP TABLE if exists `stock_item` CASCADE;
-DROP TABLE if exists `catagory` CASCADE;
+DROP TABLE IF EXISTS `employee`;
+DROP TABLE IF EXISTS `department`;
+DROP TABLE IF EXISTS `book`;
+DROP TABLE IF EXISTS `publisher`;
+DROP TABLE IF EXISTS `bookcenter`;
+DROP TABLE IF EXISTS `warehouse`;
+DROP TABLE IF EXISTS `stock_item`;
+DROP TABLE IF EXISTS `catagory`;
 
 CREATE TABLE `employee` (
+`id` bigint(11) NOT NULL AUTO_INCREMENT,
 `guid` bigint(11) NOT NULL,
-`employee_id` int(11) NOT NULL,
 `name` varchar(255) NOT NULL,
 `age` int NOT NULL,
 `gender` enum('MALE','FEMALE') NOT NULL,
 `position` varchar(255) NOT NULL,
 `salary` decimal(10,2) NOT NULL,
-PRIMARY KEY (`guid`) 
+PRIMARY KEY (`id`) 
 );
 
 CREATE TABLE `department` (
-`id` bigint(11) NOT NULL,
+`id` bigint(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
-`manager` int(11) NOT NULL,
+`manager_employee_id` bigint(11) NOT NULL,
 `address` varchar(255) NOT NULL,
 `phone` varchar(255) NOT NULL,
 PRIMARY KEY (`id`) 
 );
 
 CREATE TABLE `book` (
-`id` bigint(11) NOT NULL,
+`id` bigint(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
 `specification` varchar(255) NOT NULL,
-`publisher` bigint(255) NOT NULL,
-`catagory` bigint(255) NOT NULL,
+`publisher_id` bigint(255) NOT NULL,
+`catagory_id` bigint(255) NOT NULL,
 `cost` decimal(10,2) NOT NULL,
 `retail_price` decimal(10,2) NOT NULL,
 `wholesale_price` decimal(10,2) NOT NULL,
@@ -42,7 +40,7 @@ PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `publisher` (
-`id` bigint(11) NOT NULL,
+`id` bigint(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
 `phone` varchar(255) NOT NULL,
 `address` varchar(255) NOT NULL,
@@ -50,32 +48,32 @@ PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `bookcenter` (
-`id` bigint(11) NOT NULL,
+`id` bigint(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
-`general_manager` bigint(255) NOT NULL,
+`general_manager_employee_id` bigint(255) NOT NULL,
 PRIMARY KEY (`id`) 
 );
 
 CREATE TABLE `warehouse` (
-`id` bigint(11) NOT NULL,
+`id` bigint(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
-`manager` int(11) NOT NULL,
+`manager_employee_id` int(11) NOT NULL,
 `phone` varchar(255) NOT NULL,
 PRIMARY KEY (`id`) 
 );
 
 CREATE TABLE `stock_item` (
-`id` bigint(11) NOT NULL,
-`book` bigint(11) NOT NULL,
+`id` bigint(11) NOT NULL AUTO_INCREMENT,
+`book_id` bigint(11) NOT NULL,
 `quantity` int(255) NOT NULL,
-`warehouse` bigint(255) NOT NULL,
+`warehouse_id` bigint(255) NOT NULL,
 PRIMARY KEY (`id`) 
 );
 
 CREATE TABLE `catagory` (
-`id` bigint(11) NOT NULL,
+`id` bigint(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
-`sales_department` bigint(255) NOT NULL,
+`sales_department_id` bigint(255) NOT NULL,
 PRIMARY KEY (`id`) 
 );
 
