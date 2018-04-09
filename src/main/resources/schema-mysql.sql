6327dbc2e7f2fc565ad1ecfb7ea80bb5e738fe34
Henry Lin <badcop@163.com>
2018-04-09 08:51:12 +0800

    update

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -1,20 +1,13 @@
-ALTER TABLE `bookcenter` DROP FOREIGN KEY `fk_bookcenter_bookcenter_1`;
-ALTER TABLE `department` DROP FOREIGN KEY `fk_department`;
-ALTER TABLE `warehouse` DROP FOREIGN KEY `fk_warehouse`;
-ALTER TABLE `book` DROP FOREIGN KEY `fk_book`;
-ALTER TABLE `stock_item` DROP FOREIGN KEY `fk_stock_item`;
-ALTER TABLE `stock_item` DROP FOREIGN KEY `fk_stock_item_1`;
-ALTER TABLE `catagory` DROP FOREIGN KEY `fk_catagory`;
-ALTER TABLE `book` DROP FOREIGN KEY `fk_book_1`;
 
-DROP TABLE `employee`;
-DROP TABLE `department`;
-DROP TABLE `book`;
-DROP TABLE `publisher`;
-DROP TABLE `bookcenter`;
-DROP TABLE `warehouse`;
-DROP TABLE `stock_item`;
-DROP TABLE `catagory`;
+
+DROP TABLE if exists `employee` CASCADE;
+DROP TABLE if exists `department` CASCADE;
+DROP TABLE if exists `book` CASCADE;
+DROP TABLE if exists `publisher` CASCADE;
+DROP TABLE if exists `bookcenter` CASCADE;
+DROP TABLE if exists `warehouse` CASCADE;
+DROP TABLE if exists `stock_item` CASCADE;
+DROP TABLE if exists `catagory` CASCADE;
 
 CREATE TABLE `employee` (
 `guid` bigint(11) NOT NULL,
@@ -85,14 +78,4 @@ CREATE TABLE `catagory` (
 `sales_department` bigint(255) NOT NULL,
 PRIMARY KEY (`id`) 
 );
-
-
-ALTER TABLE `bookcenter` ADD CONSTRAINT `fk_bookcenter_bookcenter_1` FOREIGN KEY (`general_manager`) REFERENCES `employee` (`employee_id`);
-ALTER TABLE `department` ADD CONSTRAINT `fk_department` FOREIGN KEY (`manager`) REFERENCES `employee` (`employee_id`);
-ALTER TABLE `warehouse` ADD CONSTRAINT `fk_warehouse` FOREIGN KEY (`manager`) REFERENCES `employee` (`employee_id`);
-ALTER TABLE `book` ADD CONSTRAINT `fk_book` FOREIGN KEY (`publisher`) REFERENCES `publisher` (`id`);
-ALTER TABLE `stock_item` ADD CONSTRAINT `fk_stock_item` FOREIGN KEY (`book`) REFERENCES `book` (`id`);
-ALTER TABLE `stock_item` ADD CONSTRAINT `fk_stock_item_1` FOREIGN KEY (`warehouse`) REFERENCES `warehouse` (`id`);
-ALTER TABLE `catagory` ADD CONSTRAINT `fk_catagory` FOREIGN KEY (`sales_department`) REFERENCES `department` (`id`);
-ALTER TABLE `book` ADD CONSTRAINT `fk_book_1` FOREIGN KEY (`catagory`) REFERENCES `catagory` (`id`);
 
