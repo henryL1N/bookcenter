ca4e3fcafa1e1db9b25896fe0cfd95093e3be018
Henry Lin <badcop@163.com>
2018-05-27 23:13:22 +0800

    up

--- src/main/webapp/app/operations/operation.module.ts
+++ src/main/webapp/app/operations/operation.module.ts
@@ -2,14 +2,14 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { BookCenterPurchaseModule } from './purchase/purchase.module';
 import {BookCenterSaleModule } from './sale/sale.module';
-// import { BookCenterInventoryModule } from './inventory/inventory.module';
+import { BookCenterInventoryModule } from './stock/stock.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         BookCenterPurchaseModule,
         BookCenterSaleModule,
-        // BookCenterInventoryModule,
+        BookCenterStockModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
