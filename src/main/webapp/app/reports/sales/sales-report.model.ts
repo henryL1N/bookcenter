0181a685013e366622943a845809e37682999cc8
Henry Lin <badcop@163.com>
2018-06-05 16:33:39 +0800

    up

--- src/main/webapp/app/reports/sales/sales-report.model.ts
+++ src/main/webapp/app/reports/sales/sales-report.model.ts
@@ -3,9 +3,9 @@ import { BaseEntity } from './../../shared';
 export class SalesReport implements BaseEntity {
     constructor(
         public id?: number,
-        public year?: number,
-        public month?: number,
-        public profit?: number,
+        public bookId?: number,
+        public bookName?: string,
+        public quantity?: number,
     ) {
     }
 }
